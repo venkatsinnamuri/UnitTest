1cc8dbe56e37fe5c0cf05dfb6156a84667adfc53
Venkata Innamuri <venkata.innamuri@aveva.com>
2023-06-21 21:14:53 +0530

    Add project files.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3164c504-b78a-4e3a-9eb7-9191e987cb47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void BeforeTest()
+        {
+
+        }
+
+        [TestMethod]
+        [TestCategory ("DevOps") ]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+            Console.WriteLine("First Unit test case!");
+            Console.ReadLine();
+        }
+        public void AfterTest()
+        {
+
+        }
+    }
+}
